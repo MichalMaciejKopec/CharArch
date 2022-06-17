38e6587604fdb460ac953f8d62a78a519f566709
MichalMaciejKopec <michalkopec8@gmail.com>
2022-06-17 11:54:34 +0200

    główna struktura plików

---
+++ development/js/app.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+
+
+const App = () => {
+    return (
+        <h1>Test2</h1>
+    )
+}
+
+ReactDOM.render(<App/>, document.getElementById("app"));
